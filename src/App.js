dea1ddfbf830a37ff64c0edf95fbca407ee16e70
Peter Hui <huishikuen@gmail.com>
2023-01-21 15:43:34 -0800

    Create spinner and result card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import './App.css';
+import { useState } from "react";
+import ResultCard from "./components/ResultCard";
+import Spinner from "./components/Spinner";
 
 function App() {
+  const [isSearching, setIsSearching] = useState(false);
+  const [keywordInput, setKeywordInput] = useState("");
+  const [explanation, setExplanation] = useState("");
+
+  const handleClickOfSearchButton = (e) => {
+    setIsSearching(true);
+    setTimeout(() => {
+      setExplanation("Testing testing ...")
+      setIsSearching(false);
+    }, 1000)
+  }
+
+  const handleUserInputChange = (e) => {
+    setKeywordInput(e.target.value);
+  }
+
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen' style={{position: 'relative', bottom: '180px'}}>
+    <div className='flex flex-col items-center justify-center min-h-screen' style={{ position: 'relative', bottom: '180px' }}>
       <div className='text-6xl font-bold mb-6'>
         Kidspedia
       </div>
@@ -10,13 +28,38 @@ function App() {
       <div className='max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-12'>
         <form className='w-full'>
           <div className="mb-6">
-            <input type="text" id="keyword" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Search for things you want to know" required />
+            <input
+              type="text"
+              id="keyword"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+              placeholder="Search for things you want to know"
+              value={keywordInput}
+              onChange={handleUserInputChange}
+            />
           </div>
           <div className='flex flex-col items-center justify-start'>
-            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+            {isSearching
+              ? <Spinner />
+              :
+              <button
+                type="submit"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:bg-slate-200 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
+                onClick={handleClickOfSearchButton}
+              // disabled={keywordInput === ""}
+              >
+                Search
+              </button>
+            }
           </div>
         </form>
       </div>
+      {
+        !isSearching && explanation &&
+        <ResultCard
+          keyword={keywordInput}
+          explanation={explanation}
+        />
+      }
     </div>
   );
 }
---
+++ src/components/ResultCard.js
@@ -0,0 +1,11 @@
+function ResultCard({keyword, explanation}) {
+  return (
+    <div href="#" className="block w-9/12 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"># {keyword}</h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">{explanation}</p>
+    </div>
+
+  );
+}
+
+export default ResultCard;
