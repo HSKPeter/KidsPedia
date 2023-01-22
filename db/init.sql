2ed3889db5aebf6fd48d01e57f6fb3791c3f8474
Peter Hui <huishikuen@gmail.com>
2023-01-22 01:13:22 -0800

    Setup dev db

---
+++ db/init.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE kidspedia;
+
+\c kidspedia
+
+CREATE TABLE keywords (
+    keyword VARCHAR(255),
+    explanation TEXT,
+    PRIMARY KEY (keyword)
+);
+
+INSERT INTO keywords
+VALUES ("gravity", "Gravity is an invisible force that pulls objects towards each other. It is the force that makes things like apples fall from a tree. It also holds us to the Earth. Gravity is responsible for making the planets and stars stay in their orbits. You can feel gravity when you jump, because it pulls you back down to the ground. It also makes the oceans stay in their place. Gravity is a very strong force and it is always working, even when you can't see it.");
